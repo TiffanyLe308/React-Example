815e6a34c7d587d890402df4d1952624d7c44ebe
Tiffany Le <tiffanyle308@gmail.com>
2018-04-10 21:29:59 +0300

    add youtube API key

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
 import reducers from './reducers';
 
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyDrQsWL0nCDNqPCszbqbbcGQjgoHAfkO4c';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos })
+      // this.setState({ videos: videos })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello Tiffany</h1>
+        <SearchBar />
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
