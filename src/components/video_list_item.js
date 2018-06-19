dfab31819b226bf8a112d4e6e7129eaa225fc74d
Tiffany Le <tiffanyle308@gmail.com>
2018-06-19 16:34:46 +0300

    old version

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+const VideoListItem = ({video}) => {
+  console.log(video);
+
+  const imgUrl = video.snippet.thumbnails.default.url
+  const imgTitle = video.snippet.title
+
   return (
-    <li>Videos</li>
+    <li className="list-group-item">
+      <div className="video-list-media">
+        <div className="medie-left">
+          <img className="media-object" src={imgUrl} />
+        </div>
+
+        <div className="media-body">
+          <div className="media-heading">{imgTitle}
+          </div>
+        </div>
+      </div>
+    </li>
   );
 }
 
